72f167c2ca3ceb560277c957dc7896e9898f0744
juju <julien.prados@unige.ch>
2015-01-01 11:10:35 +0100

    fix bugs in the examples

--- bmrm/R/vectorialLoss.R
+++ bmrm/R/vectorialLoss.R
@@ -18,14 +18,10 @@
 #'   # -- Add a constant dimension to the dataset to learn the intercept
 #'   cst <- sqrt(max(rowSums(x*x)))
 #'   x <- cbind(x,cst)
-#'
-#'   # -- compute the loss for predicting a given category instead of the true category
-#'   l <- matrix(1,length(y),max(y))
-#'   l[cbind(seq_along(y),y)] <- 0	
 #'   
 #'   # -- train a multi-class SVM & compute the predictions
-#'   train.multiclass <- function(x,y,...) {
-#'     m <- bmrm(x,y,lossfun=softMarginVectorLoss,...)
+#'   train.multiclassSVM <- function(x,y,...) {
+#'     m <- bmrm(softMarginVectorLoss(x,y),...)
 #'     m$w <- matrix(m$w,ncol(x))
 #'     m$f <- x %*% m$w
 #'     m$y <- max.col(m$f)
@@ -34,7 +30,7 @@
 #'     return(m)    
 #'   }
 #'   # train a L1-regularized multi-class SVM
-#'   m <- train.multiclass(x,y,l=l,MAX_ITER=50,regfun='l1',LAMBDA=1)
+#'   m <- train.multiclassSVM(x,y,MAX_ITER=50,regfun='l1',LAMBDA=1)
 #'   
 #'   # -- Plot the dataset and the decision boundaries
 #'   gx <- seq(min(x[,1]),max(x[,1]),length=200) # positions of the probes on x-axis
@@ -42,10 +38,11 @@
 #'   Y <- outer(gx,gy,function(a,b){
 #'      max.col(cbind(a,b,cst) %*% m$w)
 #'   }) # matrix of predictions for all probes
-#'   layout(matrix(1:2,1))
-#'   image(gx,gy,Y,asp=1,main="dataset & decision boundaries")
+#'   layout(matrix(c(1,3,2,3),2,2))
+#'   image(gx,gy,Y,asp=1,main="dataset & decision boundaries",xlab=colnames(x)[1],ylab=colnames(x)[2])
 #'   points(x,pch=19+y)
 #'   plot(m$log$epsilon,type="o",ylab="epsilon gap",xlab="iteration")
+#'   plot(row(m$f),m$f,pch=19+col(m$f),ylab="prediction values",xlab="sample")
 softMarginVectorLoss <- function(x,y,l="0/1") {
   if (!is.matrix(x)) stop('x must be a numeric matrix')
   if (!is.integer(y)) stop('y must be an integer vector')
