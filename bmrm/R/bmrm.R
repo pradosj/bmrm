ef3a342096d7005a503b66eff07f02fb27f31ae3
juju <julien.prados@unige.ch>
2015-01-01 10:44:29 +0100

    export gradient and gradient<- mthods

--- bmrm/R/bmrm.R
+++ bmrm/R/bmrm.R
@@ -1,10 +1,16 @@
 
-
-gradient.default <- function(x) attr(x, "gradient")
+#' @export
 gradient <- function(x,...) UseMethod("gradient")
 
-"gradient<-.default" <- function(x,value) {attr(x, "gradient") <- value;x}
+#' @export
+gradient.default <- function(x) attr(x, "gradient")
+
+#' @export
 "gradient<-" <- function(x,...) UseMethod("gradient<-")
+
+#' @export
+"gradient<-.default" <- function(x,value) {attr(x, "gradient") <- value;x}
+
 
 
 
