6adddfd9957baffbbf7c4013641781f462c79a82
juju <julien.prados@unige.ch>
2015-01-14 18:53:21 +0100

    improve documentation of gradient functions

--- bmrm/R/loss.R
+++ bmrm/R/loss.R
@@ -1,14 +1,25 @@
 
-
+#' @name gradient
+#' @rdname gradient
+#' @aliases gradient.default
+#' @aliases gradient<-
+#' @aliases gradient<-.default
+#' @title Return or set gradient attribute
+#' @param x any R object
+#' @param value new gradient value to set
+#' @return attr(x,"gradient")
 #' @export
 gradient <- function(x,...) UseMethod("gradient")
 
+#' @rdname gradient
 #' @export
 gradient.default <- function(x) attr(x, "gradient")
 
+#' @rdname gradient
 #' @export
-"gradient<-" <- function(x,...) UseMethod("gradient<-")
+"gradient<-" <- function(x,value,...) UseMethod("gradient<-")
 
+#' @rdname gradient
 #' @export
 "gradient<-.default" <- function(x,value) {attr(x, "gradient") <- value;x}
 
