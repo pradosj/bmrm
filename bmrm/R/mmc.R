fa85bc9ba21bdf3a45026908890ddf7f7c3b0875
Julien Prados <prados@iMac-de-Julien.local>
2015-02-17 11:01:04 +0100

    cosmetic changes to MMC: some function renaming; improve documentation; Add N0 parameter

--- bmrm/R/mmc.R
+++ bmrm/R/mmc.R
@@ -7,7 +7,7 @@
 #' @param R numeric matrix of risks: element (i,j) is the loss penalty for assigning cluster j to sample i
 #' @param lb an integer specifying the minimum number of sample per cluster
 #' @return a binary matrix of the same dimension as R with the solution to the assignment problem
-mmcMinClusterAssign <- function(R,lb=1L) {
+mmcBestClusterAssign <- function(R,lb=1L) {
   eq <- cbind(as.vector(row(R)),seq_along(R),1)
   gt <- cbind(as.vector(col(R)) + nrow(R),seq_along(R),1)
   opt <- lp("min",
@@ -27,7 +27,7 @@ mmcMinClusterAssign <- function(R,lb=1L) {
 #' @export
 #' @param F numeric prediction matrix of the MMC model: (i,j) being prediction of the model for sample i being part of cluster j
 #' @return numeric matrix of risks where element (i,j) is the loss penalty for assigning cluster j to sample i
-mmcRisk <- function(F) {
+mmcClusterAssignRisk <- function(F) {
   R <- matrix(NA_real_,nrow(F),ncol(F))
   for(k in  1:ncol(F)) {
     xi <- pmax(1 + F - F[,k],0)
@@ -51,8 +51,8 @@ mmcLoss <- function(x, k=3L, ...) {
   function(w) {
     W <- matrix(w, ncol(x),k)
     F <- x %*% W
-    R <- mmcRisk(F)
-    Y <- mmcMinClusterAssign(R,...)
+    R <- mmcClusterAssignRisk(F)
+    Y <- mmcBestClusterAssign(R,...)
     
     G <- 1-Y+F-rowSums(F*Y)
     G <- ifelse(G>0,1,0)
@@ -68,13 +68,16 @@ mmcLoss <- function(x, k=3L, ...) {
 #' Convenient wrapper function to solve max-margin clustering problem on a dataset
 #' 
 #' Solve max-margin clustering problem with multiple random starting points to avoid being trap by local minima.
-#' The random starting points are determined by randomly assigning 3 samples to each cluster and solving for multi-class SVM
+#' The random starting points are determined by randomly assigning N0 samples to each cluster and solving for multi-class SVM
 #' 
 #' @export
 #' @param x numeric matrix representing the dataset (one sample per row)
 #' @param k an integer specifying number of clusters to find
+#' @param N0 number of instance to randomly assign per cluster when determining a random starting point.
+#'        The classification dataset it defines is used to train a multi-class SVM whose solution is used 
+#'        as the starting point of current MMC iteration.
 #' @param LAMBDA the complexity parameter for nrbm()
-#' @param NUM_RAMDOM_START number of random starting points to start with
+#' @param NUM_RAMDOM_START number of MMC iteration to perform with a random starting point
 #' @param seed the random seed basis to use
 #' @param nrbmArgsSmv arguments to nrbm() when solving for multi-class SVM problem
 #' @param nrbmArgsMmc arguments to nrbm() when solving for max-margin clustering problem
@@ -98,16 +101,16 @@ mmcLoss <- function(x, k=3L, ...) {
 #'    Y <- outer(gx,gy,function(a,b){max.col(cbind(a,b,1) %*% W)})
 #'    image(gx,gy,Y,asp=1,main="MMC clustering",xlab=colnames(x)[1],ylab=colnames(x)[2])
 #'    points(x,pch=19+y)
-mmc <- function(x,k=2L,LAMBDA=1,NUM_RAMDOM_START=50L,seed=123,nrbmArgsSmv=list(maxCP=50L,MAX_ITER=300L,LAMBDA=LAMBDA),nrbmArgsMmc=list(),...) {
+mmc <- function(x,k=2L,N0=3L,LAMBDA=1,NUM_RAMDOM_START=50L,seed=123,nrbmArgsSmv=list(maxCP=50L,MAX_ITER=300L,LAMBDA=LAMBDA),nrbmArgsMmc=list(),...) {
   nrbmArgsSmv$convexRisk <- TRUE
   nrbmArgsMmc$convexRisk <- FALSE
   nrbmArgsMmc$riskFun <- mmcLoss(x,k=k,...)
   nrbmArgsMmc$LAMBDA <- LAMBDA
   
-  models <- lapply(seq_len(NUM_RAMDOM_START),function(i) {
+  models <- mclapply(mc.cores=mc.cores,seq_len(NUM_RAMDOM_START),function(i) {
     # select a starting point w0 by randomly selecting 3 samples in each cluster and train a multi-class SVM on them
     set.seed(seed+i)
-    n0 <- min(nrow(x),k*3L)
+    n0 <- min(nrow(x),k*N0)
     i0 <- sample(seq_len(nrow(x)),n0)
     y0 <- sample(rep_len(seq_len(k),n0))
     nrbmArgsSmv$riskFun <- softMarginVectorLoss(x[i0,],y0)
