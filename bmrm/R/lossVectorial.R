00f7a104d4fc6c261d1d3e7c2e57807d3570f3ff
juju <julien.prados@unige.ch>
2017-03-30 21:41:52 +0200

    add ontologyLoss

--- bmrm/R/lossVectorial.R
+++ bmrm/R/lossVectorial.R
@@ -76,4 +76,50 @@ softMarginVectorLoss <- function(x,y,l=1 - table(seq_along(y),y)) {
 
 
 
+#' Ontology Loss Function
+#' 
+#' @param x instance matrix, where x(t,) defines the features of instance t
+#' @param y target vector where y(t) is an integer encoding target of x(t,)
+#' @param l loss matrix. l(t,p(t)) must be the loss for predicting target p(t) instead of y(t) 
+#'        for instance t. By default, the parameter is set to a 0/1 loss matrix.
+#' @param dag a numeric column oriented matrix defining the path in the Direct Acyclic Graph (DAG) to each class label
+#' @return a function taking one argument w and computing the loss value and the gradient at point w
+#' @export
+#' @references Teo et al.
+#'   A Scalable Modular Convex Solver for Regularized Risk Minimization.
+#'   KDD 2007
+#' @examples
+#'   # -- Load the data
+#'   x <- data.matrix(iris[1:2])
+#'   y <- iris$Species
+#'   dag <- matrix(c(1,0,0,0,1,1,0,1,0,0,0,1),4,byrow=TRUE)
+#'   w <- nrbm(ontologyLoss(x,y,dag=dag))
+#'   w <- matrix(w,ncol(x))
+#'   max.col(x %*% w %*% dag)
+ontologyLoss <- function(x,y,l=1 - table(seq_along(y),y),dag=diag(nlevels(y))) {
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (!is.factor(y)) stop('y must be a factor')
+  if (!is.matrix(dag)) stop('x must be a numeric matrix')
+  if (ncol(dag)!=nlevels(y)) stop('ncol(dag) should match with nlevels(y)')
+  if (ncol(dag)>nrow(dag)) stop('dag matrix must have more row than column (or equal)')
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
+  if (!identical(nrow(x),nrow(l))) stop('dimensions of x and l mismatch')
+  if (nlevels(y)!=ncol(l)) stop('ncol(l) do not match with nlevels(y)')
+  
+  function(w) {
+    w <- matrix(w,ncol(x),nrow(dag))
+    fp <- x %*% w
+    z <- fp %*% dag + l
+    Y <- max.col(z,ties.method = "first")
+    G <- dag[,Y] - dag[,y]
+    val <- sum(z[cbind(seq_along(Y),Y)] - z[cbind(seq_along(y),y)])
+    gradient(val) <- crossprod(x,t(G))
+    return(val)
+  }
+}
+
+
+
+
+
 
