5c8ce65c5a0f20a7c80d3edc430883ebfd453b9d
pradosj <julien.prados@unige.ch>
2017-04-26 11:54:48 +0200

    Reorganize the loss function in 2 files:  - loss.R with already vectorized losses  - loss_to_vectorize.R with loss function not yet vectorized

--- bmrm/R/loss.R
+++ bmrm/R/loss.R
@@ -181,3 +181,160 @@ epsilonInsensitiveRegressionLoss <- function(x,y,epsilon,loss.weights=1) {
 
 
 
+
+
+
+
+
+
+#' Hinge Loss function for SVM
+#'
+#' @param x matrix of training instances (one instance by row)
+#' @param y numeric vector of values in (-1,+1) representing the training labels for each instance in x
+#' @param loss.weights numeric vector of loss weights to incure for each instance of x in case of misprediction. 
+#'        Vector length should match length(y), but values are cycled if not of identical size. 
+#'        Default to 1 so we define a standard 0/1 loss for SVM classifier. 
+#'        The parameter might be useful to adapt SVM learning in case of unbalanced class distribution.
+#' @return a function taking one argument w and computing the loss value and the gradient at point w
+#' @export
+#' @references Teo et al.
+#'   A Scalable Modular Convex Solver for Regularized Risk Minimization.
+#'   KDD 2007
+#' @seealso bmrm
+#' @examples
+#'   x <- cbind(data.matrix(iris[1:2]),1)
+#'   y <- c(-1,1,1)[iris$Species]
+#'   w <- bmrm(hingeLoss(x,y),LAMBDA=0.1,verbose=TRUE)
+hingeLoss <- function(x,y,loss.weights=1) {
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (is.numeric(y)) {
+    if (!all(y %in% c(-1,1))) stop('y must be a numeric vector of either -1 or +1, or a 2-levels factor')
+  } else {
+    y <- as.factor(y)
+    if (nlevels(y)!=2) stop("y must have exatly 2 levels")
+    y <- c(-1,1)[y]
+  }
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
+  loss.weights <- rep(loss.weights,length.out=length(y))
+  
+  function(w) {
+    w <- cbind(matrix(numeric(),ncol(x),0),w)
+    f <- x %*% w
+    loss <- loss.weights * pmax(1-y*f,0)
+    grad <- loss.weights * (loss>0) * (-y)
+    val <- colSums(loss)
+    gradient(val) <- crossprod(x,grad)
+    return(val)
+  }
+}
+
+
+
+#' The loss function to maximize area under the ROC curve
+#' 
+#' @param x matrix of training instances (one instance by row)
+#' @param y a 2-levels factor representing the training labels for each instance in x
+#' @param loss.weights numeric vector of loss weights to incure for each instance of x. 
+#'        Vector length should match length(y), but values are cycled if not of identical size.
+#' @return a function taking one argument w and computing the loss value and the gradient at point w
+#' @export
+#' @import matrixStats
+#' @references Teo et al.
+#'   Bundle Methods for Regularized Risk Minimization
+#'   JMLR 2010
+#' @seealso bmrm
+#' @examples
+#'   x <- cbind(data.matrix(iris[1:2]),1)
+#'   y <- c(-1,1,1)[iris$Species]
+#'   w <- bmrm(rocLoss(x,y),LAMBDA=0.1,verbose=TRUE)
+rocLoss <- function(x,y,loss.weights=1) {
+  y <- as.factor(y)
+  if (nlevels(y)!=2) stop("y must have exatly 2 levels")
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
+  loss.weights <- rep(loss.weights,length.out=length(y))
+  y <- c(-1,1)[y]
+  
+  function(w) {
+    w <- cbind(matrix(numeric(),ncol(x),0),w)
+    c <- x %*% w - 0.5*y
+    o <- matrix(row(c)[order(col(c),c)],nrow(c))
+    
+    sp <- matrixStats::colCumsums(0+matrix(y[o]==+1,nrow(o)))
+    sm <- sum(y==-1) - matrixStats::colCumsums(0+matrix(y[o]==-1,nrow(o)))
+    l <- 0*o
+    l[cbind(as.vector(o),as.vector(col(o)))] <- ifelse(y[o]==-1,sp,-sm)
+    l <- l/(sum(y==-1)*sum(y==+1))
+    l <- loss.weights * l
+    
+    val <- colSums(l*c)
+    gradient(val) <- crossprod(x,l)
+    return(val)
+  }
+}
+
+
+
+#' F beta score loss function
+#'
+#' @param x matrix of training instances (one instance by row)
+#' @param y numeric vector of values in (-1,+1) representing the training labels for each instance in x
+#' @param beta a numeric value setting the beta parameter is the f-beta score
+#' @return a function taking one argument w and computing the loss value and the gradient at point w
+#' @export
+#' @references Teo et al.
+#'   A Scalable Modular Convex Solver for Regularized Risk Minimization.
+#'   KDD 2007
+#' @seealso bmrm
+#' @examples
+#'   x <- cbind(data.matrix(iris[1:2]),1)
+#'   y <- c(-1,1,1)[iris$Species]
+#'   w <- bmrm(fbetaLoss(x,y),LAMBDA=0.01,verbose=TRUE)
+fbetaLoss <- function(x,y,beta=1) {
+  y <- as.factor(y)
+  if (nlevels(y)!=2) stop("y must have exatly 2 levels")
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
+  y <- c(-1,1)[y]
+  
+  .fbeta <- function(TP,TN,P,N,beta) {
+    beta2 <- beta*beta
+    (1+beta2)*TP / (TP+N-TN+beta2*P)
+  }
+  
+  function(w) {
+    w <- cbind(matrix(numeric(),ncol(x),0),w)
+    f <- x %*% w
+    
+    o <- matrix(row(f)[order(col(f),-f)],nrow(f))
+    op <- matrix(o[y[o]==1],ncol=ncol(o))
+    on <- matrix(o[y[o]==-1],ncol=ncol(o))
+    on <- on[rev(seq(nrow(on))),,drop=FALSE]
+    p <- local({
+      F <- matrix(f[cbind(as.vector(op),as.vector(col(op)))],nrow(op))
+      2*t(colSums(F,na.rm=TRUE) - t(matrixStats::colCumsums(rbind(0,F))))
+    })
+    n <- local({
+      F <- matrix(f[cbind(as.vector(on),as.vector(col(on)))],nrow(on))
+      2*t(colSums(F,na.rm=TRUE) - t(matrixStats::colCumsums(rbind(0,F))))
+    })
+    
+    ij <- expand.grid(i=seq(nrow(p)),j=seq(nrow(n)))
+    
+    # warning: matrix R might be memory consuming
+    R <- 1 - .fbeta(ij$i-1,ij$j-1,nrow(op),nrow(on),beta) - p[ij$i,,drop=FALSE] + n[ij$j,,drop=FALSE]
+    
+    mi <- max.col(t(R),ties.method="first")
+    Y <- matrix(-y,length(y),ncol(w))
+    
+    msk <- t(t(row(Y))<ij$i[mi])
+    Y[cbind(op[msk],col(Y)[msk])] <- 1
+    
+    msk <- t(t(row(Y))<ij$j[mi])
+    Y[cbind(on[msk],col(Y)[msk])] <- -1
+    
+    val <- R[cbind(mi,seq_along(mi))]
+    gradient(val) <- crossprod(x,Y-y)
+    return(val)
+  }
+}
--- bmrm/R/loss_to_vectorize.R
+++ bmrm/R/loss_to_vectorize.R
@@ -1,4 +1,36 @@
 
+
+#' Compute or check the structure of a cost matrix 
+#' 
+#' @param y a factor representing the labels of the instances
+#' @param C either a cost matrix to check for consistency with labels in y, or a character string defining the standard matrix to compute. 
+#'        If a character string the accepted values are "0/1" for a 0-1 cost matrix or "linear" for linear cost.
+#' @return the cost matrix object
+#' @export
+#' @seealso bmrm, ordinalRegressionLoss
+costMatrix <- function(y,C=c("0/1","linear")) {
+  y <- as.factor(y)
+  if (is.character(C)) {
+    C <- match.arg(C)
+    C <- switch(C,
+                "0/1" = {
+                  C <- matrix(1,nlevels(y),nlevels(y),dimnames = list(levels(y),levels(y)))
+                  diag(C) <- 0
+                  return(C)
+                },
+                "linear" = abs(outer(levels(y),levels(y),'-'))
+    )
+  } else {
+    C <- as.matrix(C)
+    if (nrow(C)!=ncol(C)) stop("C must be a square matrix")
+    if (nlevels(y)!=nrow(C)) stop("dimension of the square matrix C doesn't match with number of levels in y")
+  }
+  return(C)
+}
+
+
+
+
 #' Soft Margin Vector Loss function for multiclass SVM
 #' 
 #' @param x instance matrix, where x(t,) defines the features of instance t
@@ -122,6 +154,102 @@ ontologyLoss <- function(x,y,l=1 - table(seq_along(y),y),dag=diag(nlevels(y))) {
 }
 
 
+
+
+
+
+#' The loss function for ordinal regression
+#' 
+#' @param x matrix of training instances (one instance by row)
+#' @param y integer vector of positive values (>=1) representing the training labels for each instance in x
+#' @param C the cost matrix to use, C[i,j] being the cost for predicting label i instead of label j.
+#' @param impl either the string "loglin" or "quadratic", that define the implementation to use for the computation of the loss.
+#' @return a function taking one argument w and computing the loss value and the gradient at point w
+#' @export
+#' @references Teo et al.
+#'   Bundle Methods for Regularized Risk Minimization
+#'   JMLR 2010
+#' @seealso bmrm
+#' @examples
+#' # -- Load the data
+#' x <- data.matrix(iris[1:4])
+#' y <- as.integer(iris$Species)
+#' 
+#' # -- Train the model
+#' w <- bmrm(ordinalRegressionLoss(x,y),LAMBDA=0.001,EPSILON_TOL=0.0001)
+#' w2 <- bmrm(ordinalRegressionLoss(x,y,impl="quadratic"),LAMBDA=0.001,EPSILON_TOL=0.0001)
+#' 
+#' # -- plot predictions
+#' f <- x %*% w
+#' f2 <- x %*% w2
+#' layout(1:2)
+#' plot(y,f)
+#' plot(f,f2,main="compare predictions of quadratic and loglin implementations")
+#' 
+#' # -- Compute accuracy
+#' ij <- expand.grid(i=seq(nrow(x)),j=seq(nrow(x)))
+#' n <- tapply(f[ij$i] - f[ij$j]>0,list(y[ij$i],y[ij$j]),sum)
+#' N <- table(y[ij$i],y[ij$j])
+#' print(n/N)
+ordinalRegressionLoss <- function(x,y,C="0/1",impl=c("loglin","quadratic")) {
+  impl <- match.arg(impl)
+  
+  # check parameters at first call
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')  
+  C <- costMatrix(y,C)
+  y <- as.integer(y)
+  m <- length(y)
+  mi <- tabulate(y,nbins=ncol(C))
+  M <- (m*m - sum(mi*mi))/2
+  C <- C / M
+  
+  .loglin <- function(w) {
+    w <- rep(w,length.out=ncol(x))
+    f <- x %*% w
+    c <- c(f-0.5,f+0.5)
+    o <- order(c)
+    
+    j <- ((o-1)%%m)+1
+    
+    l <- matrix(0,2*m,length(mi))
+    l[cbind(which(o<=m),y[j[o<=m]])] <- 1
+    l <- apply(l,2,cumsum)
+    
+    u <- matrix(0,2*m,length(mi))
+    u[cbind(which(o>m),y[j[o>m]])] <- 1
+    u <- mi[col(u)] - apply(u,2,cumsum)
+    
+    Gu <- t(C)[y[j],] * u
+    Gu[col(Gu)>=y[j]] <- 0
+    Gl <- C[y[j],] * l
+    Gl[col(Gl)<=y[j]] <- 0
+    
+    v <- ifelse(o<=m,-rowSums(Gu), rowSums(Gl))
+    r <- sum(v*c[o])
+    g <- matrix(NA,m,2)
+    g[cbind(j,1 + (o-1)%/%m)] <- v
+    g <- rowSums(g)
+    
+    gradient(r) <- crossprod(g,x)
+    return(r)
+  }
+  
+  .quadratic <- function(w) {
+    w <- rep(w,length.out=ncol(x)) 
+    f <- x %*% w
+    
+    # alternative computation in quadratic time for debugging purpose only
+    z <- expand.grid(i=factor(1:m),j=factor(1:m))
+    z <- z[y[z$i] < y[z$j],]
+    z <- z[1+f[z$i]-f[z$j]>0,]
+    R <- sum(C[cbind(y[z$i],y[z$j])] * (1+f[z$i]-f[z$j]))
+    gradient(R) <- colSums(C[cbind(y[z$i],y[z$j])] * (x[z$i,]-x[z$j,]))
+    return(R)
+  }
+  
+  switch(impl,loglin=.loglin,quadratic=.quadratic)
+}
 
 
 
