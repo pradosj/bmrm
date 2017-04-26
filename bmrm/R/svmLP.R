3b0710c43140184be67b030e301b749730d79d8b
pradosj <julien.prados@unige.ch>
2017-04-26 14:52:26 +0200

    Factorize the documentatin for svmLP functions

--- bmrm/R/svmLP.R
+++ bmrm/R/svmLP.R
@@ -1,35 +1,51 @@
 
 
-#' SVM Linear Program
+#' Linearly Programmed SVM
 #' 
-#' Solve a linear program implementing a linear SVM 
+#' Linearly Programmed L1-loss Linear Support Vector Machine with L1 regularization  
+#' 
+#' svmLP solves a linear program implementing a linear SVM 
 #' with L1 regularization and L1 loss. It solves:
 #' min_w LAMBDA*|w| + sum_i(e_i);
 #' s.t. y_i * <w.x_i> >= 1-e_i; e_i >= 0
 #' where |w| is the L1-norm of w
-#' 
-#' @param x a numeric data matrix.
-#' @param y a response factor of 2 levels for each row of x.
-#' @param LAMBDA control the regularization strength in the optimization process. This is the value used as coefficient of the regularization term.
-#' @param instance.weights a numeric vector of weight for each row of x, recycled as necessary.
-#' @return the optimized weight vector.
-#' @export
-#' @import lpSolve
+#'
+#' svmMulticlassLP solves a linear program implementing multiclass-SVM 
+#' with L1 regularization and L1 loss. It solves:
+#' min_w LAMBDA*|w| + sum_i(e_i);
+#' s.t. <w.x_i> - <w.x_j> >= 1-e_i; e_i >= 0
+#' where |w| is the L1-norm of w
+#'
+#' @name lpSVM
+#' @param x a numeric data matrix to predict
+#' @param y a response factor for each row of x. It must be a 2 levels factor for svmLP, or a >=2 levels factor for svmMulticlassLP
+#' @return the optimized weights matrix, with class svmLP
 #' @author Julien Prados
+#' 
 #' @examples
 #'   x <- cbind(100,data.matrix(iris[1:4]))
 #'   y <- iris$Species
-#'   levels(y)[3] <- levels(y)[2]
-#'   w <- svmLP(x,y)
+#'   w <- svmMulticlassLP(x,y)
 #'   table(predict(w,x),y)
-svmLP <- function(x,y,LAMBDA=1,instance.weights=1) {
+#'
+#'   w <- svmLP(x,ifelse(y=="setosa","setosa","not_setosa"))
+#'   table(predict(w,x),y)
+NULL
+
+
+
+
+#' @describeIn lpSVM linear programm solving binary-SVM with L1-regularization and L1-norm
+#' @export
+#' @import lpSolve
+svmLP <- function(x,y,LAMBDA=1,loss.weights=1) {
   y <- as.factor(y)
   if (nlevels(y)!=2) stop("nlevels(y) must be 2")
   if (nrow(x)!=length(y)) stop("length(y) must match nrow(x)")
-  instance.weights <- rep_len(instance.weights,nrow(x))
+  loss.weights <- rep_len(loss.weights,nrow(x))
   y.num <- c(-1,+1)[y]
   opt <- lp(direction = "min",
-            objective.in = c(rep(LAMBDA,2L*ncol(x)),instance.weights),
+            objective.in = c(rep(LAMBDA,2L*ncol(x)),loss.weights),
             const.mat = cbind(y.num*x,-y.num*x,diag(nrow(x))),
             const.dir = ">=",
             const.rhs = 1
@@ -43,35 +59,19 @@ svmLP <- function(x,y,LAMBDA=1,instance.weights=1) {
 }
 
 
-#' SVM Linear Program
-#' 
-#' Solve a linear program implementing multiclass-SVM 
-#' with L1 regularization and L1 loss. It solves:
-#' min_w LAMBDA*|w| + sum_i(e_i);
-#' s.t. <w.x_i> - <w.x_j> >= 1-e_i; e_i >= 0
-#' where |w| is the L1-norm of w
-#' 
-#' @param x a numeric data matrix.
-#' @param y a response factor of >=2 levels for each row of x.
-#' @param LAMBDA control the regularization strength in the optimization process. This is the value used as coefficient of the regularization term.
-#' @param instance.weights a numeric vector of weight for each row of x, recycled as necessary.
-#' @return the optimized weight matrix
+
+
+#' @describeIn lpSVM linear programm solving multiclass-SVM with L1-regularization and L1-norm
 #' @export
 #' @import lpSolve
-#' @author Julien Prados
-#' @examples
-#'   x <- cbind(100,data.matrix(iris[1:4]))
-#'   y <- iris$Species
-#'   w <- svmMulticlassLP(x,y)
-#'   table(predict(w,x),y)
-svmMulticlassLP <- function(x,y,LAMBDA=1,instance.weights=1) {
+svmMulticlassLP <- function(x,y,LAMBDA=1,loss.weights=1) {
   y <- as.factor(y)
   if (nlevels(y)<2) stop("nlevels(y) must be >=2")
   if (nrow(x)!=length(y)) stop("length(y) must match nrow(x)")
-  instance.weights <- rep_len(instance.weights,nrow(x))
+  loss.weights <- rep_len(loss.weights,nrow(x))
 
   opt <- lp(direction = "min",
-            objective.in = c(rep(LAMBDA,2L*ncol(x)*nlevels(y)),instance.weights),
+            objective.in = c(rep(LAMBDA,2L*ncol(x)*nlevels(y)),loss.weights),
             const.mat = local({
               L <- merge(data.frame(i=seq_along(y),y=y),data.frame(Y=factor(levels(y),levels(y))))
               L <- L[L$y != L$Y,]
@@ -99,14 +99,12 @@ svmMulticlassLP <- function(x,y,LAMBDA=1,instance.weights=1) {
 
     
 
-#' Predict function for svmLP models
-#'
+
+#' @rdname lpSVM
 #' @param object an object of class svmLP
-#' @param x data.matrix to predict
 #' @param ... unused, present to satisfy the generic predict() prototype
-#' @return prediction for row of x, with an attribute "decision.value"
+#' @return predict() return predictions for row of x, with an attribute "decision.value"
 #' @export
-#' @author Julien Prados
 predict.svmLP <- function(object,x,...) {
   f <- x %*% object
   y <- colnames(f)[max.col(f,ties.method = "first")]
