22057dd779ca385e93b0e3fd1bb63a3e8a26202d
juju <julien.prados@unige.ch>
2017-04-27 23:08:59 +0200

    Fix error in logisticLoss gradient error, rename the function and improve its documentation

--- bmrm/R/loss.R
+++ bmrm/R/loss.R
@@ -108,39 +108,6 @@ epsilonInsensitiveRegressionLoss <- function(x,y,epsilon,loss.weights=1) {
 
 
 
-#' The loss function to perform a logistic regression
-#' 
-#' @param x matrix of training instances (one instance by row)
-#' @param y numeric vector of values representing the training labels for each instance in x
-#' @param loss.weights numeric vector of loss weights to incure for each instance of x. 
-#'        Vector length should match length(y), but values are cycled if not of identical size.
-#' @return a function taking one argument w and computing the loss value and the gradient at point w
-#' @export
-#' @references Teo et al.
-#'   Bundle Methods for Regularized Risk Minimization
-#'   JMLR 2010
-#' @seealso bmrm
-#' @examples
-#'   x <- cbind(data.matrix(iris[1:2]),1)
-#'   y <- c(-1,1,1)[iris$Species]
-#'   w <- bmrm(logisticRegressionLoss(x,y),LAMBDA=1,verbose=TRUE)
-logisticRegressionLoss <- function(x,y,loss.weights=1) {
-  if (!is.matrix(x)) stop('x must be a numeric matrix')
-  if (!is.numeric(y)) stop('y must be a numeric vector')
-  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
-  loss.weights <- rep(loss.weights,length.out=length(y))
-  
-  function(w) {
-    w <- cbind(matrix(numeric(),ncol(x),0),w)
-    f <- x %*% w
-    loss <- loss.weights * log(1+exp(-y*f))
-    grad <- loss.weights * -y/(1+exp(-y*f))
-    val <- colSums(loss)
-    gradient(val) <- crossprod(x,grad)
-    return(val)
-  }
-}
-
 
 
 
@@ -170,6 +137,7 @@ logisticRegressionLoss <- function(x,y,loss.weights=1) {
 #'   x <- cbind(intercept=100,data.matrix(iris[1:2]))
 #'   y <- ifelse(iris$Species=="setosa","setosa","not_setosa")
 #'   w <- bmrm(hingeLoss(x,y))
+#'   w <- bmrm(logisticLoss(x,y))
 #'   w <- bmrm(rocLoss(x,y))
 #'   w <- bmrm(fbetaLoss(x,y))
 NULL
@@ -197,7 +165,29 @@ hingeLoss <- function(x,y,loss.weights=1) {
 }
 
 
-#' @describeIn binaryClassificationLosses Linear classifier maximize area under its ROC curve
+#' @describeIn binaryClassificationLosses logistic regression
+#' @export
+logisticLoss <- function(x,y,loss.weights=1) {
+  y <- as.factor(y)
+  if (nlevels(y)!=2) stop("y must have exatly 2 levels")
+  if (!is.matrix(x)) stop('x must be a numeric matrix')
+  if (nrow(x) != length(y)) stop('dimensions of x and y mismatch')
+  loss.weights <- rep(loss.weights,length.out=length(y))
+  y <- c(-1,1)[y]
+  
+  function(w) {
+    w <- cbind(matrix(numeric(),ncol(x),0),w)
+    f <- x %*% w
+    loss <- loss.weights * log(1+exp(-y*f))
+    grad <- loss.weights * y*(1/(1+exp(-y*f)) - 1)
+    val <- colSums(loss)
+    gradient(val) <- crossprod(x,grad)
+    return(val)
+  }
+}
+
+
+#' @describeIn binaryClassificationLosses Find linear weights maximize area under its ROC curve
 #' @export
 #' @import matrixStats
 rocLoss <- function(x,y,loss.weights=1) {
