5b97595671043cd8cf41fb2df1f9584d6edaf112
pradosj <julien.prados@unige.ch>
2017-07-11 12:18:46 +0200

    Fix remaing Note message in the check

--- bmrm/R/loss.R
+++ bmrm/R/loss.R
@@ -325,7 +325,8 @@ predict.fbetaLoss <- function(object,x,...) {
 #'   gx <- seq(min(x[,2]),max(x[,2]),length=200) # positions of the probes on x-axis
 #'   gy <- seq(min(x[,3]),max(x[,3]),length=200) # positions of the probes on y-axis
 #'   Y <- outer(gx,gy,function(a,b) {predict(w,cbind(100,a,b))})
-#'   image(gx,gy,unclass(Y),asp=1,main="dataset & decision boundaries",xlab=colnames(x)[2],ylab=colnames(x)[3])
+#'   image(gx,gy,unclass(Y),asp=1,main="dataset & decision boundaries",
+#'         xlab=colnames(x)[2],ylab=colnames(x)[3])
 #'   points(x[,-1],pch=19+as.integer(y))
 softMarginVectorLoss <- function(x,y,l=1 - table(seq_along(y),y)) {
   if (!is.matrix(x)) stop('x must be a numeric matrix')
