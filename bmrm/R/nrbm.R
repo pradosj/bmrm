964975e0ceee0a86c3bc77772379d6681620a9d0
pradosj <julien.prados@unige.ch>
2017-07-11 11:24:22 +0200

    Split nrbm() in 2 methods: a L2 and a L1 version

--- bmrm/R/nrbm.R
+++ bmrm/R/nrbm.R
@@ -4,6 +4,7 @@
 #' Use algorithm of Do and Artieres, JMLR 2012 to find w minimizing: 
 #' f(w) = 0.5*LAMBDA*l2norm(w) + riskFun(w)
 #' where riskFun is either a convex or a non-convex risk function.
+#' @name nrbm
 #' @param riskFun the risk function to use in the optimization (e.g.: hingeLoss, softMarginVectorLoss). 
 #'   The function must evaluate the loss value and its gradient for a given point vector (w).
 #'   The function must return the given point vector w, with attributes "lvalue" and "gradient" set.
@@ -18,7 +19,6 @@
 #' @param convexRisk a length 1 logical telling if the risk function riskFun is convex. 
 #'    If TRUE, use CRBM algorithm; if FALSE use NRBM algorithm from Do and Artieres, JMLR 2012
 #' @param LowRankQP.method a single character value defining the method used by LowRankQP (should be either "LU" or "CHOL")
-#' @param regularizer 
 #' @return the optimal weight vector (w)
 #' @references Do and Artieres
 #'   Regularized Bundle Methods for Convex and Non-Convex Risks
@@ -35,9 +35,9 @@
 #'
 #'   # -- train scalar prediction models with maxMarginLoss and fbetaLoss
 #'   models <- list(
-#'     svm_L1 = nrbm(hingeLoss(x,iris$Species=="setosa"),LAMBDA=0.1,reg='l1'),
-#'     svm_L2 = nrbm(hingeLoss(x,iris$Species=="setosa"),LAMBDA=0.1,reg='l2'),
-#'     f1_L1 = nrbm(fbetaLoss(x,iris$Species=="setosa"),LAMBDA=0.01,reg='l1')
+#'     svm_L1 = nrbmL1(hingeLoss(x,iris$Species=="setosa"),LAMBDA=0.1),
+#'     svm_L2 = nrbm(hingeLoss(x,iris$Species=="setosa"),LAMBDA=0.1),
+#'     f1_L1 = nrbmL1(fbetaLoss(x,iris$Species=="setosa"),LAMBDA=0.01)
 #'   )
 #'
 #'   # -- Plot the dataset and the predictions
@@ -58,20 +58,20 @@
 #'   Y <- X%*%beta + rnorm(nrow(X))
 #'   w <- nrbm(ladRegressionLoss(X/100,Y/100),maxCP=50)
 #'   barplot(as.vector(w))
-nrbm <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L,convexRisk=TRUE,LowRankQP.method="LU",regularizer=c("l2","l1")) {
+NULL
+
+
+#' @describeIn nrbm original L2-regularized version of nrbm
+#' @export
+nrbm <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L,convexRisk=TRUE,LowRankQP.method="LU") {
   # check parameters
   if (maxCP<3) stop("maxCP should be >=3")
-  regularizer <- match.arg(regularizer)
-  if (regularizer=="l1" && !convexRisk) stop("l1 regularizer only support convex risk")
 
   # intialize first point estimation
   w <- riskFun(w0)
   at <- as.vector(gradient(w))
   bt <- as.vector(lvalue(w)) - crossprod(as.vector(w),at)
-  switch(regularizer,
-         l1 = {f <- LAMBDA*sum(abs(w)) + lvalue(w)},
-         l2 = {f <- LAMBDA*0.5*crossprod(as.vector(w)) + lvalue(w)}
-  )
+  f <- LAMBDA*0.5*crossprod(as.vector(w)) + lvalue(w)
   st <- 0
   
   # initialize aggregated working plane and working set
@@ -80,40 +80,19 @@ nrbm <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L
   ub <- f;ub.w <- w;ub.t <- 2
   for (i in 1:MAX_ITER) {
     
-    switch(regularizer,
-           l1 = {
-             # optimize underestimator
-             opt <- lp(compute.sens = 1, direction = "max",
-                       objective.in = c(-1,rep_len(-LAMBDA,2L*ncol(A))),
-                       const.mat = cbind(-1,A,-A), const.dir = rep("<=",nrow(A)),const.rhs = -b
-             )
-             if (opt$status!=0) warning("issue in the LP solver:",opt$status)
-             opt$W <- matrix(opt$solution[-1L],ncol=2L)
-             alpha <- opt$duals[1:nrow(A)]
-             
-             # compute the optimum point and corresponding objective value    
-             w <- opt$W[,1L] - opt$W[,2L]
-             lb <- -opt$objval
-             
-             # estimate loss at the new underestimator optimum
-             w <- riskFun(w)
-             f <- LAMBDA*sum(abs(w)) + lvalue(w)
-           },
-           l2 = {
-             # optimize underestimator
-             H <- matrix(0,1L+nrow(A),1L+nrow(A))
-             H[-1,-1] <- tcrossprod(A)
-             alpha <- LowRankQP(H,c(0,-LAMBDA*b),matrix(1,1L,nrow(H)),1,rep(1,nrow(H)),method=LowRankQP.method)$alpha[-1L]
-             
-             # compute the optimum point and corresponding objective value
-             w <- as.vector(-crossprod(A,alpha) / LAMBDA)
-             lb <- LAMBDA*0.5*crossprod(as.vector(w)) + max(0,A %*% as.vector(w) + b)
-             
-             # estimate loss at the new underestimator optimum
-             w <- riskFun(w)
-             f <- LAMBDA*0.5*crossprod(as.vector(w)) + lvalue(w)
-           }
-    )
+     # optimize underestimator
+     H <- matrix(0,1L+nrow(A),1L+nrow(A))
+     H[-1,-1] <- tcrossprod(A)
+     alpha <- LowRankQP(H,c(0,-LAMBDA*b),matrix(1,1L,nrow(H)),1,rep(1,nrow(H)),method=LowRankQP.method)$alpha[-1L]
+     
+     # compute the optimum point and corresponding objective value
+     w <- as.vector(-crossprod(A,alpha) / LAMBDA)
+     lb <- LAMBDA*0.5*crossprod(as.vector(w)) + max(0,A %*% as.vector(w) + b)
+     
+     # estimate loss at the new underestimator optimum
+     w <- riskFun(w)
+     f <- LAMBDA*0.5*crossprod(as.vector(w)) + lvalue(w)
+
     # update inactivity score
     inactivity.score <- inactivity.score + pmax(1-alpha,0)
 
@@ -122,7 +101,6 @@ nrbm <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L
     bt <- lvalue(w) - crossprod(as.vector(w),at)
 
     if (!convexRisk) {
-      # L2 regularization only
       # solve possible conflicts with the new cutting plane
       if (f<ub) {
         st <- 0
@@ -175,4 +153,78 @@ nrbm <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L
 
 
 
+
+
+
+
+#' @describeIn nrbm L1-regularized version of nrbm that can only handle convex risk
+#' @export
+nrbmL1 <- function(riskFun,LAMBDA=1,MAX_ITER=1000L,EPSILON_TOL=0.01,w0=0,maxCP=50L) {
+  # check parameters
+  if (maxCP<3) stop("maxCP should be >=3")
+
+  # intialize first point estimation
+  w <- riskFun(w0)
+  at <- as.vector(gradient(w))
+  bt <- as.vector(lvalue(w)) - crossprod(as.vector(w),at)
+  f <- LAMBDA*sum(abs(w)) + lvalue(w)
+
+  # initialize aggregated working plane and working set
+  A <- rbind(at,at);b <- c(bt,bt);
+  inactivity.score <- c(NA_real_,NA_real_)
+  ub <- f;ub.w <- w;ub.t <- 2
+  for (i in 1:MAX_ITER) {
+     # optimize underestimator
+     opt <- lp(compute.sens = 1, direction = "max",
+               objective.in = c(-1,rep_len(-LAMBDA,2L*ncol(A))),
+               const.mat = cbind(-1,A,-A), const.dir = rep("<=",nrow(A)),const.rhs = -b
+     )
+     if (opt$status!=0) warning("issue in the LP solver:",opt$status)
+     opt$W <- matrix(opt$solution[-1L],ncol=2L)
+     alpha <- opt$duals[1:nrow(A)]
+     
+     # compute the optimum point and corresponding objective value    
+     w <- opt$W[,1L] - opt$W[,2L]
+     lb <- -opt$objval
+     
+     # estimate loss at the new underestimator optimum
+     w <- riskFun(w)
+     f <- LAMBDA*sum(abs(w)) + lvalue(w)
+             
+    # update inactivity score
+    inactivity.score <- inactivity.score + pmax(1-alpha,0)
+    
+    # deduce parameters of the new cutting plane
+    at <- as.vector(gradient(w))
+    bt <- lvalue(w) - crossprod(as.vector(w),at)
+
+    # update aggregated cutting plane
+    A[1,] <- alpha %*% A
+    b[1] <- alpha %*% b
+    
+    # add the new cutting plane to the working set
+    if (nrow(A)<maxCP) {
+      A <- rbind(A,at);b <- c(b,bt);
+      inactivity.score <- c(inactivity.score,0)
+      t <- length(b)
+    } else {
+      t <- which.max(inactivity.score)
+      A[t,] <- at;b[t] <- bt;
+      inactivity.score[t] <- 0
+    }
+    
+    # if new best found
+    if (f<ub) {
+      inactivity.score[ub.t] <- 0
+      ub <- f;ub.w <- w;ub.t <- t
+      inactivity.score[ub.t] <- NA
+    }
+    
+    # test end of the convergence
+    cat(sprintf("%d:gap=%g obj=%g reg=%g risk=%g w=[%g,%g]\n",i,ub-lb,ub,LAMBDA*sum(abs(ub.w)),lvalue(ub.w),min(ub.w),max(ub.w)))
+    if (ub-lb < EPSILON_TOL) break
+  }
+  if (i >= MAX_ITER) warning('max # of itertion exceeded')
+  return(ub.w)
+}
 
