02e3988c0be8528dbb9f00dcaf995ef4e101f54c
Julien Prados <prados@CL-C02W60EKHTDH.local>
2018-03-14 13:08:17 +0100

    add documentation for ... argument in rowmean

--- bmrm/R/misc.R
+++ bmrm/R/misc.R
@@ -3,7 +3,8 @@
 #' 
 #' Similar to rowsum, but for mean values.
 #' @param x a matrix
-#' @param group a factor with one element per row of x.
+#' @param group a factor with one element per row of x
+#' @param ... additional arguments are passed to rowsum()
 #' @return a matrix containing the means, with one row per level of group.
 #' @export
 rowmean <- function(x,group,...) {
